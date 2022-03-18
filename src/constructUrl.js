fb9b5d7fc9b4d8dc6cadbc320f31d02c0dae314b
taiga-tech <taiga.dev.n@gmail.com>
2022-03-18 19:04:44 +0900

    Create constructUrl.js

---
+++ src/constructUrl.js
@@ -0,0 +1,27 @@
+/**
+ * Usage
+ * **************************
+ * const myFunction = () => {
+ * const baseUrl = 'https://example.com'
+ * const params = { id: 1, name: 'fuga' }
+ * const url = constructUrl(baseUrl, params)
+ *
+ * console.log(url)
+ * // 'https://example.com?id=1&name=fuga'
+ * }
+ * **************************
+ */
+
+function constructUrl(baseUrl, params) {
+  const p = { ...params }
+
+  const query = Object.keys(p)
+    .reduce((prev, cur) => {
+      prev.push(`${cur}=${p[cur]}`)
+      return prev
+    }, [])
+    .join('&')
+
+  Logger.log('constructUrl: ' + `${baseUrl}?${query}`)
+  return `${baseUrl}?${query}`
+}
