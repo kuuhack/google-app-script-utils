f4e5d2afaa6c21c82e19a0c91ce59a06b4554a88
taiga-tech <taiga.dev.n@gmail.com>
2022-03-18 19:04:52 +0900

    Create createObject.js

---
+++ src/createObject.js
@@ -0,0 +1,24 @@
+/**
+ * Usage
+ * **************************
+ * const myFunction = () => {
+ * const sheet = SpreadsheetApp.getActiveSheet()
+ * const values = sheet.getDataRange().getValues()
+ * const object = createObjects(values)
+ *
+ * console.log(object)
+ * //  { hoge: 10, fuga: 20, piyo: 20 }
+ * }
+ * **************************
+ */
+
+function createObject(values) {
+  const object = {}
+
+  values.map((item) => {
+    object[item[0]] = item[1]
+  })
+
+  console.log(object)
+  return object
+}
