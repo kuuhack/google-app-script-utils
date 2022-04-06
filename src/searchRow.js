514ec19e5429ffbf356e7f5b53c8b6b7025c1004
taiga-tech <taiga.dev.n@gmail.com>
2022-04-06 22:24:50 +0900

    Create searchRow.js

---
+++ src/searchRow.js
@@ -0,0 +1,23 @@
+/**
+ * Usage
+ * **************************
+ *  const myFunction = () => {
+ *    const sheet = SpreadsheetApp.getActiveSheet()
+ *    const cell = searchCell(sheet, 'fuga')
+ *
+ *    console.log(cell)
+ *    // [1]
+ *  }
+ * **************************
+ */
+
+function searchRow(sheet, value) {
+  const cells = sheet.createTextFinder(value).matchEntireCell(true).findAll()
+
+  const rows = cells.map((cell) => {
+    Logger.log('セル位置: ' + cell.getA1Notation())
+    return cell.getRow()
+  })
+
+  return rows
+}
