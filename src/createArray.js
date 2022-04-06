a63469ba8ade41df799c529d6d13197332b2d46f
taiga-tech <taiga.dev.n@gmail.com>
2022-04-06 22:16:43 +0900

    Update createArray.js

--- src/createArray.js
+++ src/createArray.js
@@ -4,14 +4,14 @@
  *  const myFunction = () => {
  *  const sheet = SpreadsheetApp.getActiveSheet()
  *  const array = createArray(sheet)
- * 
+ *
  *  console.log(array)
- *  // { hoge: 10, fuga: 20, piyo: 20 }
+ *  // [{ hoge: 10, fuga: 20, piyo: 20 }, { hoge: 30, fuga: 40, piyo: 40 }]
  *  }
  * **************************
  */
 
-const createArray = (sheet) => {
+function createArray(sheet) {
   const values = sheet.getDataRange().getValues()
   const objects = createObjects(values)
   Logger.log('Index Length: ' + objects.length)
