421e63c3a7f018e32cc81ffea7cca0bbdbec68d3
taiga-tech <taiga.dev.n@gmail.com>
2023-01-17 12:59:03 +0900

    Update createArray.js

--- src/createArray.js
+++ src/createArray.js
@@ -1,16 +1,16 @@
 /**
- * Usage
- * **************************
- * const myFunction = () => {
+ * ## Usage
+ *
+ * ```js
+ * function myFunction() {
  *   const sheet = SpreadsheetApp.getActiveSheet()
  *   const array = createArray(sheet)
  *
  *   console.log(array)
  *   // [{ hoge: 10, fuga: 20, piyo: 20 }, { hoge: 30, fuga: 40, piyo: 40 }]
  * }
- * **************************
+ * ```
  */
-
 function createArray(sheet) {
   const values = sheet.getDataRange().getValues()
   const objects = createObjects(values)
